ebf7c6aa549f27798a89813381665c1e66336222
yikang <yikang@divisacapital.com>
2018-01-18 16:34:08 -0500

    adding markdown feature

--- article/views.py
+++ article/views.py
@@ -5,15 +5,17 @@ from django.template import loader
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.views import generic
+import markdown
 from datetime import datetime
 # Create your views here.
-def home(request):
-    post_list = Article.objects.all()  #获取全部的Article对象
-    return render(request, 'home.html', {'post_list' : post_list})
+# def home(request):
+#     post_list = Article.objects.all()  #获取全部的Article对象
+#     return render(request, 'home.html', {'post_list' : post_list})
 
 
 def detail(request, pk):
     post = get_object_or_404(Article, pk=pk)
+    post.content = markdown.markdown(post.content, ['extra','codehilite','toc'])
     return render(request, 'detail.html', {'post' : post})
 
 
@@ -21,9 +23,9 @@ def index(request):
     article_lsit = Article.objects.all().order_by('-pub_date')
     return render(request, 'index.html', {'post_list':article_lsit})
 
-class DetailView(generic.DeleteView):
-    model = Article
-    template_name = 'detail.html'
+# class DetailView(generic.DeleteView):
+#     model = Article
+#     template_name = 'detail.html'
 
 # def test(request) :
 #     return render(request, 'test.html', {'current_time': datetime.now()})
