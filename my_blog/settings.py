9aabf2a300773b545c31c7531961717b9ec6d814
yikang <yikang@divisacapital.com>
2018-01-25 14:55:02 -0500

    zh-hans

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -116,7 +116,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'zh-CN'
+LANGUAGE_CODE = 'zh-Hans'
 
 TIME_ZONE = 'UTC'
 
