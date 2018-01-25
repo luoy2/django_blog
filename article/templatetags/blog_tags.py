fa81ced54c877ef9b38fa81cb0f2b76ea056aafd
yikang <yikang@divisacapital.com>
2018-01-25 18:11:12 -0500

    category count

--- article/templatetags/blog_tags.py
+++ article/templatetags/blog_tags.py
@@ -1,5 +1,6 @@
 from ..models import *
 from django import template
+from django.db.models.aggregates import Count
 
 
 register=template.Library()
@@ -15,8 +16,9 @@ def archives():
 
 @register.simple_tag
 def get_categories():
-    return Category.objects.all()
-
+    # 记得在顶部引入 count 函数
+    # Count 计算分类下的文章数，其接受的参数为需要计数的模型的名称
+    return Category.objects.annotate(num_posts=Count('post')).filter(num_posts__gt=0)
 
 
 @register.simple_tag
